d637a53460f293ef05787d0663024860642d0617
NahuMeza <nahuelgmeza@gmail.com>
2024-08-17 02:01:03 -0300

    exercises 1-3

---
+++ lab0/lab0.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# EJERCICIO 1
+cat /proc/cpuinfo | grep "model name"
+# EJERCICIO 2
+cat /proc/cpuinfo | grep "model name" | wc -l
+# EJERCICIO 3
+wget https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv && cat -s heroes.csv | awk -F';' '{print $2}' | awk 'NF' | awk '{print tolower($0)}' | tr -d ' ' 
