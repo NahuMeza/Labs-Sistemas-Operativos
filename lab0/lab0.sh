415955272e792428e064f3ca5983dff7ad84f3be
NahuMeza <nahuelgmeza@gmail.com>
2024-08-17 02:03:50 -0300

    moved heroes.csv

--- lab0/lab0.sh
+++ lab0/lab0.sh
@@ -6,3 +6,4 @@ cat /proc/cpuinfo | grep "model name"
 cat /proc/cpuinfo | grep "model name" | wc -l
 # EJERCICIO 3
 wget https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv && cat -s heroes.csv | awk -F';' '{print $2}' | awk 'NF' | awk '{print tolower($0)}' | tr -d ' ' 
+# EJERCICIO 4
