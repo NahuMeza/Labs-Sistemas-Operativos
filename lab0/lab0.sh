a373789bc078eaad90d9566125c015373506de97
NahuMeza <nahuelgmeza@gmail.com>
2024-08-17 21:55:50 -0300

    ej4

--- lab0/lab0.sh
+++ lab0/lab0.sh
@@ -7,3 +7,8 @@ cat /proc/cpuinfo | grep "model name" | wc -l
 # EJERCICIO 3
 wget https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv && cat -s heroes.csv | awk -F';' '{print $2}' | awk 'NF' | awk '{print tolower($0)}' | tr -d ' ' 
 # EJERCICIO 4
+ #Dia de mayor temperatura
+sort -k 5nr weather_cordoba.in | head -n 1| awk -F' ' '{print $1, $2, $3}'
+ #Dia de menor temperatura
+sort -k 6n weather_cordoba.in | head -n 1| awk -F' ' '{print $1, $2, $3}'
+#EJERCICIO 5 
