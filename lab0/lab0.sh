8d6b94cc966aa9dcfd9bda1a904792385eb37539
NahuMeza <nahuelgmeza@gmail.com>
2024-08-18 17:26:58 -0300

    lab0 - finished

--- lab0/lab0.sh
+++ lab0/lab0.sh
@@ -18,3 +18,10 @@ awk -F' ' '{print $0, $7 - $8}' superliga.in | sort -k 2nr -k 9nr | awk -F' ' '{
 #EJERCICIO 7
 ip address | grep "link/ether"
 #EJERCICIO 8
+mkdir subcaps && for i in {01..10}; do touch ./subcaps/"fma_S01E${i}_es.srt"; done
+cd subcaps
+for i in *_es.srt; do mv "$i" "${i/_es.srt/.srt}"; done;
+#EJERCICIO 9
+ #a
+ffmpeg -ss 00:00:02 -to 00:00:07 -i sistops-2024-08-18_12.11.37.mp4 -c copy copy.mp4 #alguna forma de hacerlo sin saber la duracion del video?
+ffmpeg -i voice.mp3 -i music.mp3 -filter_complex "[0:a]volume=1.3[a1]; [1:a]volume=0.5[a2]; [a1][a2]amix=inputs=2:duration=shortest:dropout_transition=3[out]" -map "[out]" podcast.mp3
